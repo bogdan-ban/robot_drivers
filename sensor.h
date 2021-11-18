2ecbfcf7ea2b3768d18da988abcf88cd3344880f
darius-man <91048623+darius-man@users.noreply.github.com>
2021-11-18 16:13:25 +0200

    sensor interface

---
+++ sensor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdio.h>
+#include "communication.h"
+
+class Sensor
+{
+public:
+	Sensor(Communication* _communication): communication(_communication) {}
+
+	virtual void start_communication() = 0;
+	virtual void stop_communication() = 0;
+	virtual uint8_t* read_data(int size) = 0;
+	virtual void send_command(uint8_t* command) = 0;
+	virtual void publish_message() = 0;
+	virtual void create_message() = 0;
+
+	~Sensor() 
+	{
+		stop_communication();
+	}
+
+private:
+	Communication* communication;
+	void* message;
+};
